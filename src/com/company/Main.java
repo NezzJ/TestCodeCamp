5131f6f4ffac91d6a81e28d32872c404d75cabca
NezzJ <obe.onene@gmail.com>
2022-03-15 22:29:15 -0700

    added my java project file

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.println("Hello world");
+    System.out.println("It's my first java project and I am learning how to use java for web test automation.");
+    System.out.println("I am learning software testing with Testify Limited and it has been an enlightening journey so far.");
+    System.out.println("I have learnt manual software testing, automated software testing using postman and selenium.");
+    System.out.println("I can prepare Test Plan, Testcase Documentation, Test Summary Report etc.");
+    }
+}
